87dabcdafd84232ba6cb4201d17f3f93545a98fd
rico <rico@bincentive.com>
2019-08-27 17:29:14 +0800

    add hello k8s resource

--- eks_cluster/eks_cluster_stack.py
+++ eks_cluster/eks_cluster_stack.py
@@ -14,3 +14,39 @@ class EksClusterStack(core.Stack):
             default_capacity=2,
             default_capacity_instance=ec2.InstanceType('t2.medium')
         )
+
+        cluster.add_resource('hello-kubernetes',
+          {
+            "apiVersion": "v1",
+            "kind": "Service",
+            "metadata": { "name": "hello-kubernetes" },
+            "spec": {
+              "type": "LoadBalancer",
+              "ports": [ { "port": 80, "targetPort": 8080 } ],
+              "selector": { "app": "hello-kubernetes" }
+            }
+          },
+          {
+            "apiVersion": "apps/v1",
+            "kind": "Deployment",
+            "metadata": { "name": "hello-kubernetes" },
+            "spec": {
+              "replicas": 1,
+              "selector": { "matchLabels": { "app": "hello-kubernetes" } },
+              "template": {
+                "metadata": {
+                  "labels": { "app": "hello-kubernetes" }
+                },
+                "spec": {
+                  "containers": [
+                    {
+                      "name": "hello-kubernetes",
+                      "image": "paulbouwer/hello-kubernetes:1.5",
+                      "ports": [ { "containerPort": 8080 } ]
+                    }
+                  ]
+                }
+              }
+            }
+           }
+        )
